e10029a2bf9d484aa6c3cccb00bbca09fb4d9485
Alihamza <i222566@nu.edu.pk>
2025-11-23 20:48:03 +0500

    refactor: Clear __all__ exports in collection modules

--- src/collection/__init__.py
+++ src/collection/__init__.py
@@ -1,5 +1,3 @@
 """Data Collection Layer"""
 
-from src.collection.collectors.base_collector import BaseCollector, CollectionConfig
-
-__all__ = ['BaseCollector', 'CollectionConfig']
+__all__ = []
--- src/collection/collectors/__init__.py
+++ src/collection/collectors/__init__.py
@@ -1,7 +1,3 @@
 """Collectors for various ad platforms"""
 
-from src.collection.collectors.base_collector import BaseCollector, CollectionConfig
-from src.collection.collectors.meta_ad_library import MetaAdLibraryCollector
-from src.collection.collectors.web_scraper import WebScraperCollector
-
-__all__ = ['BaseCollector', 'CollectionConfig', 'MetaAdLibraryCollector', 'WebScraperCollector']
+__all__ = []
